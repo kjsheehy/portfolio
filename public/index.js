a97372f83111a8d896521df4c506ec6033cefdd0
kjsheehy <kjsheehy@gmail.com>
2023-07-05 09:53:23 -0400

    Removed (unused) modal JS and CSS; minor CSS layout changes in preparation for mobile-friendly updates

--- public/index.js
+++ public/index.js
@@ -1,18 +1,18 @@
 'use strict';
 
-const warEl = document.getElementById('war');
-const brdngEl = document.getElementById('brdng');
-const projectModalEl = document.getElementById('project-modal');
+// const warEl = document.getElementById('war');
+// const brdngEl = document.getElementById('brdng');
+// const projectModalEl = document.getElementById('project-modal');
 
-function modalShow(projectName) {
-  console.log('Hovered over war div');
-  projectModalEl.classList.remove('hidden');
-}
+// function modalShow(projectName) {
+//   console.log('Hovered over war div');
+//   projectModalEl.classList.remove('hidden');
+// }
 
-function modalHide() {
-  console.log('mouseout');
-  projectModalEl.classList.add('hidden');
-}
+// function modalHide() {
+//   console.log('mouseout');
+//   projectModalEl.classList.add('hidden');
+// }
 
-warEl.addEventListener('mouseover', modalShow);
-projectModalEl.addEventListener('mouseout', modalHide);
+// warEl.addEventListener('mouseover', modalShow);
+// projectModalEl.addEventListener('mouseout', modalHide);
